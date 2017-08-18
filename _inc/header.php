e79b4bc70ff66e5441b0bd47ae51628182e8806a
Gregory Thomason <gregory_thomason@hotmail.com>
2017-08-18 15:00:22 -0500

    Update header.php

--- _inc/header.php
+++ _inc/header.php
@@ -21,6 +21,7 @@
 
     	<!-- 3rd party scripts -->
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
+	<script src="//cdn.polyfill.io/v2/polyfill.min.js"></script>
 	<script src="js/modernizr.js" type="text/javascript"></script>
 	<script src="js/flexslider/jquery.flexslider.js" type="text/javascript"></script>
 	
