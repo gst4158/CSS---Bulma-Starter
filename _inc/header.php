6576def1c6c6f900656510e505aa0476053f1411
Gregory Thomason <gregory_thomason@hotmail.com>
2017-08-28 11:20:47 -0500

    Update header.php

--- _inc/header.php
+++ _inc/header.php
@@ -4,34 +4,34 @@
 <html lang="en-US" class="no-js">
 <head>
 	<meta charset="UTF-8">
-	<title>Bulma Starter Pack</title>
+	<title>Monkeysports Bulma</title>
 
 	<link href="//www.google-analytics.com" rel="dns-prefetch">
-    	<link href="favicon.ico" rel="shortcut icon">
+    <link href="favicon.ico" rel="shortcut icon">
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Bulma Testing Playground">
+	<meta name="description" content="Monkeysports Testing Playground">
 
 	<meta name="author" content="Gregory Thomason" />
 	<meta name="copyright" content="© 2017 Gregory Thomason" />
 
-    	<!-- styles -->
-    	<link href="css/style.nested.css" rel="stylesheet" type="text/css">
+    <!-- styles -->
+    <link href="css/style.nested.css" rel="stylesheet" type="text/css">
 
-    	<!-- 3rd party scripts -->
-    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
+    <!-- 3rd party scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
 	<script src="//cdn.polyfill.io/v2/polyfill.min.js"></script>
 	<script src="js/modernizr.js" type="text/javascript"></script>
 	<script src="js/flexslider/jquery.flexslider.js" type="text/javascript"></script>
-	
+
 	<!-- site scripts -->
-	<script src="js/bulma_extend.js" type="text/javascript"></script>
+	<script src="js/bulma_extend_v2.js" type="text/javascript"></script>
 
 </head>
 <body>
 <div id="top" class="content-wrapper">
-	
+
 <nav class="navbar ">
 	<div class="navbar-brand">
 		<a class="navbar-item" href="/">
@@ -53,7 +53,7 @@
 				<a class="navbar-link" href="#">
 					Dropdown Example
 				</a>
-				<div class="navbar-dropdown">
+				<div class="navbar-dropdown menu-active">
 					<a class="navbar-item " href="/">
 						<i class="fa fa-home margin-right-5" aria-hidden="true"></i>
 						Home
@@ -64,7 +64,7 @@
 					</a>
 				</div>
 			</div>
-			
+
 			<div class="navbar-item has-dropdown is-hoverable">
 				<a class="navbar-link" href="#">
 					Dropdown Example s
@@ -90,4 +90,3 @@
 
 	</div>
 </nav>
-
